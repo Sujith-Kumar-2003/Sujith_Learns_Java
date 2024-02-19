13c609dd1fb1d866c01636ed2e6494edc13bd369
Sujith <94170693+luna-martinez@users.noreply.github.com>
2024-02-19 01:37:08 -0500

    Fiel_read and write.

---
+++ src/File_read_write.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class File_read_write{
+    public static void main(String[] args) {
+        // Specify the path to your file
+        String filePath = "/Users/deBauch_Casanova/Downloads/Sujith_learns_Java/src/new.txt";
+        String f = "/Users/deBauch_Casanova/Downloads/Sujith_learns_Java/src/out.txt";
+
+        try {
+            FileReader fileReader = new FileReader(filePath);
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            FileWriter fileWriter = new FileWriter(f);
+
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.write("Hello, this is your new file content!");
+
+
+            bufferedWriter.close();
+
+            System.out.println("File created successfully at: " + filePath);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
