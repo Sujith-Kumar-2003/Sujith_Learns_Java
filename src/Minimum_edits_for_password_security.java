0fc4c553a482f9cde42f9a951385cc2525967d46
Sujith-Kumar-2003 <sujithkumar2003k@gmail.com>
2025-01-16 02:09:48 -0500

    Made a few files

--- src/Minimum_edits_for_password_security.java
+++ src/Minimum_edits_for_password_security.java
@@ -1,26 +1,61 @@
 import java.util.Scanner;
-import java.util.*;
+
 public class Minimum_edits_for_password_security {
-    public static void main(String [] args){
-        System.out.print("Enter the number of passwords in int: ");
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int no_of_password = sc.nextInt();
-        System.out.println("All the passwords must be 10 characters long");
 
-        String [][] strings = new String[1][no_of_password];
-        strings[0][0] = sc.next();
-        strings[0][1] = sc.next();
-        strings[0][2] = sc.next();
-        strings[0][3] = sc.next();
-        strings[0][4] = sc.next();
+        String[] passwords = new String[no_of_password];
+        for (int i = 0; i < no_of_password; i++) {
+            passwords[i] = sc.next();
+        }
+
+        int[] editArray = new int[no_of_password];
+
+        for (int i = 0; i < no_of_password; i++) {
+            String password = passwords[i];
+            int editsRequired = 0;
+
+            // Condition 1: Check if the password length is exactly 10
+            if (password.length() != 10) {
+                editsRequired += Math.abs(password.length() - 10);
+
+            }
+
+            // Condition 2: Check for at least one uppercase letter
+            if (!password.matches(".*[A-Z].*")) {
+                editsRequired++;
+            }
+
+            // Condition 3: Check for at least one lowercase letter
+            if (!password.matches(".*[a-z].*")) {
+                editsRequired++;
+            }
+
+            // Condition 4: Check for at least one digit
+            if (!password.matches(".*[0-9].*")) {
+                editsRequired++;
+            }
+
+            // Condition 5: Check for at least one special character (@, #, $, %, &, *)
+            if (!password.matches(".*[@#$%&*].*")) {
+                editsRequired++;
+            }
+
+            editArray[i] = editsRequired;
+        }
+        int minimum = 10;
+        String name = "";
+        for ( int i = 0 ; i < no_of_password; i ++){
+            if (editArray[i] < minimum){
+                minimum = editArray[i];
+                name = passwords[i];
+            }
+        }
+        System.out.println(name);
+        System.out.println(minimum);
 
 
-        System.out.println(Arrays.toString(strings[0]));
+        sc.close();
     }
 }
-
-//pass123
-//Abcdefghij
-//1234567890
-//abc$5678
-//P@ssw0rd1
