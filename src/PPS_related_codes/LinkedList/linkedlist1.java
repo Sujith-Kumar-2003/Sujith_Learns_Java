bdfa7bed0de5078f7856245b925e43ffbf048728
Sujith-Kumar-2003 <sujithkumar2003k@gmail.com>
2025-04-07 10:53:17 -0400

    Added a few Features

---
+++ src/PPS_related_codes/LinkedList/linkedlist1.java
@@ -0,0 +1,21 @@
+package PPS_related_codes.LinkedList;
+import java.io.*;
+import java.util.Scanner;
+import java.util.*;
+
+
+
+//Methods to create,
+//1.addtostart
+//2.addtoend
+//3.showListContents
+//4.replace
+//5.insertAfter
+//6.insertBefore
+
+
+public class linkedlist1 {
+    Scanner sc = new Scanner(System.in);
+
+
+}
